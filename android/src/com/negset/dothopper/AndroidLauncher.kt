5a0f6ed7a88cea109711bbfded29d969247ea65c
negset <negset1858@gmail.com>
2019-10-22 18:46:11 +0900

    convert to kotlin

---
+++ android/src/com/negset/dothopper/AndroidLauncher.kt
@@ -0,0 +1,14 @@
+package com.negset.dothopper
+
+import android.os.Bundle
+
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        initialize(DotHopper(), config)
+    }
+}
